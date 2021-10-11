ad13a0ee2a7bf86f46cc86674a6f675a60c7d68d
Daniel Mohr <daniel.mohr@dlr.de>
2021-10-11 12:29:52 +0200

    enhanced documentation

--- src/py_fuse_git_bare_fs/repotools_dulwich.py
+++ src/py_fuse_git_bare_fs/repotools_dulwich.py
@@ -1,7 +1,7 @@
 """
 :Author: Daniel Mohr
 :Email: daniel.mohr@dlr.de
-:Date: 2021-10-08 (last change).
+:Date: 2021-10-11 (last change).
 :License: GNU GENERAL PUBLIC LICENSE, Version 2, June 1991.
 """
 
@@ -10,6 +10,24 @@ import dulwich.repo
 
 
 def get_ref(src_dir, root_object):
+    """
+    This use the python module dulwich to read/handle a git repository.
+
+    You can dynamically choose using dulwich or git, e. g.:
+
+      try:
+          from py_fuse_git_bare_fs.repotools_dulwich import get_ref
+      except ModuleNotFoundError:
+          from py_fuse_git_bare_fs.repotools_git import get_ref
+
+    :param src_dir: path to the git repository as str
+    :param root_object: name of the branch as bytes
+    :return: hash of the branch root_object of the repository src_dir as str
+             or error message as bytes
+
+    :Author: Daniel Mohr
+    :Date: 2021-10-11
+    """
     refs_root_object = b'refs/heads/' + root_object
     try:
         repo = dulwich.repo.Repo(src_dir)
@@ -19,4 +37,4 @@ def get_ref(src_dir, root_object):
     if refs_root_object in refs:
         return repo.get_refs()[refs_root_object].decode()
     else:
-        return ''
+        return root_object + b' missing'
--- src/py_fuse_git_bare_fs/repotools_git.py
+++ src/py_fuse_git_bare_fs/repotools_git.py
@@ -9,6 +9,17 @@ import subprocess
 
 
 def get_ref(src_dir, root_object):
+    """
+    This use the command line program git to read/handle a git repository.
+
+    :param src_dir: path to the git repository as str
+    :param root_object: name of the branch as bytes
+    :return: hash of the branch root_object of the repository src_dir as str
+             or error message as bytes
+
+    :Author: Daniel Mohr
+    :Date: 2021-10-08
+    """
     cpi = subprocess.run(
         ["git cat-file --batch-check='%(objectname)'"],
         input=root_object,
