95cb0738db8f6ff72cee24c82c0bca18c5939a0b
Daniel Mohr <daniel.mohr@dlr.de>
2023-03-31 18:34:48 +0200

    enhanced coding style

--- tests/script_fuse_git_bare_fs_tree_annex.py
+++ tests/script_fuse_git_bare_fs_tree_annex.py
@@ -1,7 +1,7 @@
 """
 :Author: Daniel Mohr
 :Email: daniel.mohr@dlr.de
-:Date: 2022-02-03
+:Date: 2023-03-31
 :License: GNU GENERAL PUBLIC LICENSE, Version 2, June 1991.
 
 tests the script 'fuse_git_bare_fs tree' regarding git-annex files
@@ -30,16 +30,30 @@ import time
 import unittest
 
 
+def _terminate_wait_kill(cpi, timeout=3, sleepbefore=None, sleepafter=None):
+    """
+    :Author: Daniel Mohr
+    :Date: 2022-01-13
+    """
+    if sleepbefore is not None:
+        time.sleep(sleepbefore)
+    cpi.terminate()
+    cpi.wait(timeout=timeout)
+    cpi.kill()
+    if sleepafter is not None:
+        time.sleep(sleepafter)
+
+
 class ScriptFuseGitBareFsTreeAnnex(unittest.TestCase):
     """
     :Author: Daniel Mohr
-    :Date: 2022-02-23
+    :Date: 2023-03-31
     """
 
     def test_fuse_git_bare_fs_tree_annex(self):
         """
         :Author: Daniel Mohr
-        :Date: 2022-02-23
+        :Date: 2023-03-31
         """
         # pylint: disable=invalid-name
         serverdir = 'server'
@@ -54,61 +68,61 @@ class ScriptFuseGitBareFsTreeAnnex(unittest.TestCase):
                     'data', 'create_git_annex_test_env')
             shutil.copy(src_file_name,
                         os.path.join(tmpdir, 'create_git_annex_test_env'))
-            cp = subprocess.run(
+            subprocess.run(
                 [os.path.join(tmpdir, 'create_git_annex_test_env')],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 shell=True, cwd=tmpdir,
                 timeout=6, check=True)
             # run tests
-            cp = subprocess.Popen(
+            with subprocess.Popen(
                 ['exec ' + 'fuse_git_bare_fs tree ' + serverdir + ' ' +
                  mountpointdir],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                shell=True, cwd=tmpdir)
-            t0 = time.time()
-            while time.time() - t0 < 3:  # wait up to 3 seconds for mounting
-                # typical it needs less than 0.4 seconds
-                if bool(os.listdir(os.path.join(tmpdir, mountpointdir))):
-                    break
-                time.sleep(0.1)
-            self.assertEqual(
-                set(os.listdir(
-                    os.path.join(tmpdir, mountpointdir))),
-                {'repo1', 'repo2', 'repo3'})
-            # test content of repo1:
-            repo1 = os.path.join(tmpdir, mountpointdir, 'repo1')
-            self.assertEqual(set(os.listdir(repo1)), {'f1', 'f2', 'f3'})
-            for filename in ['f1', 'f2']:
-                with open(os.path.join(repo1, filename)) as fd:
+                    shell=True, cwd=tmpdir) as cpi:
+                t0 = time.time()
+                while time.time() - t0 < 3:
+                    # wait up to 3 seconds for mounting
+                    # typical it needs less than 0.4 seconds
+                    if bool(os.listdir(os.path.join(tmpdir, mountpointdir))):
+                        break
+                    time.sleep(0.1)
+                self.assertEqual(
+                    set(os.listdir(
+                        os.path.join(tmpdir, mountpointdir))),
+                    {'repo1', 'repo2', 'repo3'})
+                # test content of repo1:
+                repo1 = os.path.join(tmpdir, mountpointdir, 'repo1')
+                self.assertEqual(set(os.listdir(repo1)), {'f1', 'f2', 'f3'})
+                for filename in ['f1', 'f2']:
+                    joinedpath = os.path.join(repo1, filename)
+                    with open(joinedpath, encoding='utf-8') as fd:
+                        data = fd.read()
+                    self.assertEqual(data, filename + '\n')
+                with self.assertRaises(FileNotFoundError):
+                    joinedpath = os.path.join(repo1, 'f3')
+                    with open(joinedpath, encoding='utf-8') as fd:
+                        data = fd.read()
+                # test content of repo2:
+                repo2 = os.path.join(tmpdir, mountpointdir, 'repo2', 'repo2')
+                self.assertEqual(set(os.listdir(repo2)), {'f1', 'f2', 'f3'})
+                # check f1 is link:
+                self.assertEqual(
+                    os.lstat(os.path.join(repo2, 'f1')).st_mode, 16877)
+                # check git annex file in subdirectory,
+                # e. g.: f1 -> ../.git/annex/objects/...
+                joinedpath = os.path.join(repo2, 'f1', 'f1')
+                with open(joinedpath, encoding='utf-8') as fd:
                     data = fd.read()
-                self.assertEqual(data, filename + '\n')
-            with self.assertRaises(FileNotFoundError):
-                with open(os.path.join(repo1, 'f3')) as fd:
-                    data = fd.read()
-            # test content of repo2:
-            repo2 = os.path.join(tmpdir, mountpointdir, 'repo2', 'repo2')
-            self.assertEqual(set(os.listdir(repo2)), {'f1', 'f2', 'f3'})
-            # check f1 is link:
-            self.assertEqual(
-                os.lstat(os.path.join(repo2, 'f1')).st_mode, 16877)
-            # check git annex file in subdirectory,
-            # e. g.: f1 -> ../.git/annex/objects/...
-            with open(os.path.join(repo2, 'f1', 'f1')) as fd:
-                data = fd.read()
-            self.assertEqual(data, 'f1\n')
-            # check f2 is executable:
-            self.assertEqual(os.lstat(os.path.join(repo2, 'f2')).st_mode,
-                             33261)
-            # check f3 is symbolic link:
-            self.assertEqual(
-                os.lstat(os.path.join(repo2, 'f3')).st_mode, 41471)
-            # clean up:
-            cp.terminate()
-            cp.wait(timeout=6)
-            cp.kill()
-            cp.stdout.close()
-            cp.stderr.close()
-            cp = subprocess.run(
+                self.assertEqual(data, 'f1\n')
+                # check f2 is executable:
+                self.assertEqual(os.lstat(os.path.join(repo2, 'f2')).st_mode,
+                                 33261)
+                # check f3 is symbolic link:
+                self.assertEqual(
+                    os.lstat(os.path.join(repo2, 'f3')).st_mode, 41471)
+                # clean up:
+                _terminate_wait_kill(cpi, timeout=6)
+            subprocess.run(
                 ['chmod -R +w *'],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 shell=True, cwd=tmpdir, timeout=6, check=True)
