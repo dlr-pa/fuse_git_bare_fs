8a82e105b299f163400ab93991fc661df0c288da
Daniel Mohr <daniel.mohr@dlr.de>
2023-04-04 18:32:05 +0200

    typo

--- tests/script_fuse_git_bare_fs_repo.py
+++ tests/script_fuse_git_bare_fs_repo.py
@@ -178,7 +178,7 @@ class ScriptFuseGitBareFsRepo(
                 _terminate_wait_kill(cpi, sleepbefore=3)
                 _, cpistderr = cpi.communicate()
                 self.assertEqual(1, cpi.returncode)  # error return
-                print('cpistderr.decode() = "%s"' % cpistderr.decode())
+                print(f'cpistderr.decode() = "{cpistderr.decode()}"')
                 self.assertTrue(cpistderr.decode().startswith(
                     'fuse: bad mount point'))
             # run test: nofail
